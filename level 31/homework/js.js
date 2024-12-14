73515c2aca3466c0950eddf0387868d2c8dff41b
გაბრიელ კალანდაძე <gabikalanddze@gmail.com>
2024-12-14 20:41:46 +0400

    second commit

---
+++ level 31/homework/js.js
@@ -0,0 +1,3 @@
+function AlertFunction(){
+    alert("I Told You Im Prankster \n OK im serius now click down")
+}
---
+++ level 35/homework/main.py.py
@@ -0,0 +1,21 @@
+print(type("hello, world!"))
+
+
+print(2 > 3)
+
+
+print
+
+
+
+...
+
+
+...
+...
+
+...
+...
+
+...
+
